a9e461f572260a6587073fa9ca3e3c4c1607761d
jaywangYA <e34013160@gmail.com>
2024-05-24 00:50:50 +0800

    update settings.json, yapf, isort, flask8

--- trading_system/core/models.py
+++ trading_system/core/models.py
@@ -1,3 +1,189 @@
 from django.db import models
 
 # Create your models here.
+
+
+#null=True for data layer，blank=True for validation layer。
+class OptionData(models.Model):
+    option_id = models.AutoField(primary_key=True)
+    year = models.IntegerField()
+    month = models.IntegerField()
+    date = models.CharField(max_length=10)  # YYYY-MM-DD
+    day = models.CharField(max_length=10)  # Mon, Tue, Wed, Thu, Fri
+    tw_trade_call_count = models.IntegerField()
+    tw_trade_call_amount = models.IntegerField()
+    fr_trade_call_count = models.IntegerField()
+    fr_trade_call_amount = models.IntegerField()
+    tw_trade_put_count = models.IntegerField()
+    tw_trade_put_amount = models.IntegerField()
+    fr_trade_put_count = models.IntegerField()
+    fr_trade_put_amount = models.IntegerField()
+    call_count = models.IntegerField()
+    call_amount = models.IntegerField()
+    put_count = models.IntegerField()
+    put_amount = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)  # created time
+    updated_at = models.DateTimeField(auto_now=True)  # updated time
+
+    def __str__(self):
+        return f'{self.date} - {self.day} - {self.option_id}'
+
+
+class DayPrice(models.Model):
+    day_price_id = models.AutoField(primary_key=True)
+    year = models.IntegerField()
+    month = models.IntegerField()
+    date = models.CharField(max_length=10)  # YYYY-MM-DD
+    day = models.CharField(max_length=10)  # Mon, Tue, Wed, Thu, Fri
+    open = models.IntegerField()
+    high = models.IntegerField()
+    low = models.IntegerField()
+    close = models.IntegerField()
+    volume = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)  # created time
+    updated_at = models.DateTimeField(auto_now=True)  # updated time
+
+    def __str__(self):
+        return f'{self.date} - {self.day} - {self.day_price_id}'
+
+
+class NightPrice(models.Model):
+    night_price_id = models.AutoField(primary_key=True)
+    year = models.IntegerField()
+    month = models.IntegerField()
+    date = models.CharField(max_length=10)  # YYYY-MM-DD
+    day = models.CharField(max_length=10)  # Mon, Tue, Wed, Thu, Fri
+    open = models.IntegerField()
+    high = models.IntegerField()
+    low = models.IntegerField()
+    close = models.IntegerField()
+    volume = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)  # created time
+    updated_at = models.DateTimeField(auto_now=True)  # updated time
+
+    def __str__(self):
+        return f'{self.date} - {self.day} - {self.night_price_id}'
+
+
+class Settlement(models.Model):
+    settlement_id = models.AutoField(primary_key=True)
+    year = models.IntegerField()
+    month = models.IntegerField()
+    date = models.CharField(max_length=10)  # YYYY-MM-DD
+    day = models.CharField(max_length=10)  # Mon, Tue, Wed, Thu, Fri
+    created_at = models.DateTimeField(auto_now_add=True)  # created time
+    updated_at = models.DateTimeField(auto_now=True)  # updated time
+
+    def __str__(self):
+        return f'{self.date} - {self.day} - {self.settlement_id}'
+
+
+class Signal(models.Model):
+    option_data = models.ForeignKey(OptionData,
+                                    on_delete=models.CASCADE,
+                                    related_name='Signals')
+    signal_id = models.AutoField(primary_key=True)
+    year = models.IntegerField()
+    month = models.IntegerField()
+    date = models.CharField(max_length=10)  # YYYY-MM-DD
+    day = models.CharField(max_length=10)  # Mon, Tue, Wed, Thu, Fri
+    settlement_signal = models.IntegerField(
+        default=0)  # default 0, settlement day is 1
+    reverse_signal = models.IntegerField(
+        default=0)  # default 0, 1st day after settlement is 1
+    trading_signal = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)  # created time
+    updated_at = models.DateTimeField(auto_now=True)  # updated time
+
+    def __str__(self):
+        return f'{self.date} - {self.day} - {self.signal_id}'
+
+
+class Order(models.Model):
+    signal_data = models.ForeignKey(Signal,
+                                    on_delete=models.CASCADE,
+                                    related_name='Orders')
+    order_id = models.AutoField(primary_key=True)
+    year = models.IntegerField()
+    month = models.IntegerField()
+    date = models.CharField(max_length=10)  # YYYY-MM-DD
+    day = models.CharField(max_length=10)  # Mon, Tue, Wed, Thu, Fri
+    prodcut = models.CharField(max_length=20)
+    quantity = models.IntegerField()
+    action = models.CharField(max_length=20)  # buy, sell, no action
+    deal_price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)  # created time
+    updated_at = models.DateTimeField(auto_now=True)  # updated time
+
+    def __str__(self):
+        return f'{self.date} - {self.day} - {self.order_id}'
+
+
+class Revenue(models.Model):
+    signal_data = models.ForeignKey(Signal,
+                                    on_delete=models.CASCADE,
+                                    related_name='Revenues')
+    order_data = models.ForeignKey(Order,
+                                   on_delete=models.CASCADE,
+                                   related_name='Revenues')
+    revenue_id = models.AutoField(primary_key=True)
+    year = models.IntegerField()
+    month = models.IntegerField()
+    date = models.CharField(max_length=10)  # YYYY-MM-DD
+    day = models.CharField(max_length=10)  # Mon, Tue, Wed, Thu, Fri
+    prodcut = models.CharField(max_length=20)
+    quantity = models.IntegerField()
+    action = models.CharField(max_length=20)  # buy, sell, no action
+    open_price = models.IntegerField()
+    close_price = models.IntegerField()
+    revenue = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)  # created time
+    updated_at = models.DateTimeField(auto_now=True)  # updated time
+
+    def __str__(self):
+        return f'{self.date} - {self.day} - {self.revenue_id}'
+
+
+class Backtest(models.Model):
+    option_data = models.ForeignKey(OptionData,
+                                    on_delete=models.CASCADE,
+                                    related_name='Backtests')
+    signal_data = models.ForeignKey(Signal,
+                                    on_delete=models.CASCADE,
+                                    related_name='Backtests')
+    backtest_id = models.AutoField(primary_key=True)
+    year = models.IntegerField()
+    month = models.IntegerField()
+    date = models.CharField(max_length=10)  # YYYY-MM-DD
+    day = models.CharField(max_length=10)  # Mon, Tue, Wed, Thu, Fri
+    open = models.IntegerField()
+    high = models.IntegerField()
+    low = models.IntegerField()
+    close = models.IntegerField()
+    diff_high = models.IntegerField()
+    diff_low = models.IntegerField()
+    diff_close = models.IntegerField()
+    tw_trade_call_count = models.IntegerField()
+    tw_trade_call_amount = models.IntegerField()
+    fr_trade_call_count = models.IntegerField()
+    fr_trade_call_amount = models.IntegerField()
+    tw_trade_put_count = models.IntegerField()
+    tw_trade_put_amount = models.IntegerField()
+    fr_trade_put_count = models.IntegerField()
+    fr_trade_put_amount = models.IntegerField()
+    call_count = models.IntegerField()
+    call_amount = models.IntegerField()
+    put_count = models.IntegerField()
+    put_amount = models.IntegerField()
+    settlement_signal = models.IntegerField()
+    reverse_signal = models.IntegerField(
+        default=0)  # default 0, 1st day after settlement is 1
+    trading_signal = models.IntegerField()
+    result = models.IntegerField()
+    money_status = models.CharField(max_length=10)  # loss, gain, even
+    max_loss = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)  # created time
+    updated_at = models.DateTimeField(auto_now=True)  # updated time
+
+    def __str__(self):
+        return f'{self.date} - {self.day} - {self.backtest_id}'
--- trading_system/trading_system/settings.py
+++ trading_system/trading_system/settings.py
@@ -16,7 +16,6 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -28,7 +27,6 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -72,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "trading_system.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -86,7 +83,6 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -106,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -118,8 +113,6 @@ TIME_ZONE = 'Asia/Taipei'
 USE_I18N = True
 
 USE_TZ = True
-
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
