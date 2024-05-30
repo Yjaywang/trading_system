4a690a44f58e9f4a9f082fe36fdf91f6685a66e7
jaywangYA <e34013160@gmail.com>
2024-05-31 00:41:30 +0800

    turn on close open task cron, disable celery debug

--- trading_system/trading_system/celery.py
+++ trading_system/trading_system/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trading_system.settings')
 
@@ -10,9 +11,8 @@ app = Celery('trading_system')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # auto discover task
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-
-@app.task(bind=True, ignore_result=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
+# @app.task(bind=True, ignore_result=True)
+# def debug_task(self):
+#     print(f'Request: {self.request!r}')
